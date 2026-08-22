d90bb831f6e28a374062d9dd1ffed5d7a40c7795
Rayder819 <nicolas.0901.r@gmail.com>
2026-08-22 12:08:49 -0400

    j

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ const video = document.querySelector('.hero-video');
 const hero = document.querySelector('.hero');
 const hamburger = document.querySelector('.hamburguesa');
 const menu = document.querySelector('nav.menu ul');
+const poster = document.querySelector('.hero-poster');
 
 hero.addEventListener('mouseenter', function() {
     video.play();
+    poster.style.display = 'none';
 });
 
 hero.addEventListener('mouseleave', function() {
     video.pause();
     video.currentTime = 0;
+    poster.style.display = 'block';
 });
 
 hero.addEventListener('touchstart', function() {
     video.play();
+    poster.style.display = 'none';
 });
 
 hero.addEventListener('touchend', function() {
     video.pause();
     video.currentTime = 0;
+    poster.style.display = 'block';
 });
 
 hamburger.addEventListener('click', function() {
